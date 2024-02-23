72d69c2df0102632216100f304c932b4c805ebc6
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-02-23 12:41:45 -0500

    draw debug shapes added

--- Source/Slash/Private/Items/MyItem.cpp
+++ Source/Slash/Private/Items/MyItem.cpp
@@ -22,13 +22,26 @@ void AMyItem::BeginPlay()
 	UWorld* World = GetWorld();
 	FVector Location = GetActorLocation();
 	FVector Forward = GetActorForwardVector();
+	FVector Right = GetActorRightVector();
+	FVector Up = GetActorUpVector();
 
 	if (World)
 	{
 		// Define and show debug objects for this instanced item
-		FColor Color = FColor::Cyan;
+		FColor Color = FColor::Silver;
+		FColor ColorForward = FColor::Green;
+		FColor ColorRight = FColor::Red;
+		FColor ColorUp = FColor::Blue;
+
 		DRAW_SPHERE(Location, Color);
-		DRAW_LINE(Location, Location + Forward * 100.f, Color);
+
+		// Using individual calls to set up line + endpoint for forward vector
+		DRAW_LINE(Location, Location + Forward * 100.f, ColorForward);
+		DRAW_POINT(Location + Forward * 100.f, ColorForward);
+
+		// Using single call to draw vectors
+		DRAW_VECTOR(Location, Location + Right * 50.f, ColorRight);
+		DRAW_VECTOR(Location, Location + Up * 50.f, ColorUp);
 	}
 }
 
--- Source/Slash/Slash.h
+++ Source/Slash/Slash.h
@@ -7,3 +7,9 @@
 // Defining functions for drawing debug objects that can be used anywhere
 #define DRAW_SPHERE(Location, Color) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, Color, true);
 #define DRAW_LINE(StartLocation, EndLocation, Color) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, Color, true, -1.f, 0, 1.f);
+#define DRAW_POINT(Location, Color) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, Color, true);
+#define DRAW_VECTOR(StartLocation, EndLocation, Color) if (GetWorld()) \
+	{ \
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, Color, true, -1.f, 0, 1.f); \
+		DrawDebugPoint(GetWorld(), EndLocation, 15.f, Color, true); \
+	}
